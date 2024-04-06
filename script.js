75a6c461a1d8fab4ca104201d1c328dabc75cb5a
Roger Santos <124369449+rogerr23@users.noreply.github.com>
2024-04-05 22:43:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,10 +13,33 @@ function calcularIMC(){
 
     let imc = peso / (altura*altura);
 
+    if(imc <= 18.5){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está com magreza!</h3>`;
+    }
+
+    else if(imc >= 18.6 && imc < 25){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está normal!</h3>`
+    }
+
+    else if(imc >= 25 && imc < 30){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está sobrepeso!</h3>`
+    }
+
+    else if(imc >= 30 && imc < 35){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está com obesidade!</h3>`
+    }
+
+    else if(imc >= 35 && imc < 40){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está com obesidade grave, procure um médico!</h3>`
+    }
+
+    else if(imc > 39.5){
+        resultado.innerHTML = `<h3> Seu IMC é ${imc.toFixed(1)} \n Você está com obesidade grave 2, procure um médico!</h3>`
+    }
     //alert(imc.toFixed(1));
 
     //Escrevendo um conteudo HTML
-    resultado.innerHTML = `<h3> Seu IMC ficou ${imc.toFixed(1)}</h3>`;
+    //resultado.innerHTML = `<h3> Seu IMC ficou ${imc.toFixed(1)}</h3>`;
 
 
     }
